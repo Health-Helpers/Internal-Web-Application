2198d0bded934433c53ea2b1a48d989baecab812
solacarol <sola.carol@gmail.com>
2015-12-11 00:36:51 +0100

    ADD INDEX

--- src/cat/ehh/web/controller/LoginController.java
+++ src/cat/ehh/web/controller/LoginController.java
@@ -11,16 +11,34 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class LoginController {
 
+	/*
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String checkLogin(ModelMap model,HttpServletRequest request) {
 		if(request.getSession().getAttribute("username")!=null && !request.getSession().getAttribute("username").equals("")){
 			return "/welcome";	
 		}else{
-			return "login";
+			return "index";
 		}	
 	}
+	*/
 
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String redirectWelcomePage(ModelMap model,HttpServletRequest request) {
+		return "index";
+	}
+	
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String redirectIndex(ModelMap model,HttpServletRequest request) {
+		return "index";
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String redirectLogin(ModelMap model,HttpServletRequest request) {
+		return "login";
+	}
+	
+	
+	@RequestMapping(value="/login", method=RequestMethod.POST)
 	public String doLogin(ModelMap model,HttpServletRequest request,HttpServletResponse response) {
 		try{
 			String username = (String) request.getParameter("username");
@@ -34,11 +52,11 @@ public class LoginController {
 				return "welcome";
 			}else{
 				request.getSession().setAttribute("badLogin",true);
-				return "login";
+				return "index";
 			}
 		}catch (Exception e){
 			e.printStackTrace();
-			return "login";
+			return "index";
 		}
 	}
 
