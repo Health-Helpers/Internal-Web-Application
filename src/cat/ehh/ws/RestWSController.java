9c51ab4a90e0a232a9968979ab989d07fa16f585
navijo <navijo@gmail.com>
2015-12-16 12:20:05 +0100

    RestController config

--- src/cat/ehh/ws/RestWSController.java
+++ src/cat/ehh/ws/RestWSController.java
@@ -1,5 +1,17 @@
 package cat.ehh.ws;
 
-public class RestWSController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+@RequestMapping( value = "/RestWSController" )
+public class RestWSController{
+
+	@RequestMapping( value = "/sayHello", method = RequestMethod.GET )
+	@ResponseBody
+	public String sayHello( ){
+		return "Hello Guy!";
+	}
 }
